1afa00a7f840041b4672fc9968899c6f0452fe36
alilopes <aliomar.lopesneto@gmail.com>
2022-10-20 18:20:32 -0300

    project converted to EJS

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,7 +1,15 @@
+const ProductModel = require("../models/product")
+
 const productController = {
     mostrar: (request, response) => {
+        ProductModel.getProducts();
         response.render('produto');
     },
 };
+
+function product(req, res) {
+    const productList = ProductModel.getProducts();
+    res.render("product", {productList});
+}
 
 module.exports = productController;
---
+++ src/models/product.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+function ProductModel(path, category, picture1, picture2, picture3, picture4, name, discount, oldPrice, price, paymentConditions, description, quantity) {
+  this.category = category
+  this.path = path
+  this.picture1 = picture1;
+  this.picture2 = picture2;
+  this.picture3 = picture3;
+  this.picture4 = picture4;
+  this.name = name;
+  this.discount = discount;
+  this.oldPrice = oldPrice;
+  this.price = price;
+  this.paymentConditions = paymentConditions;
+  this.description = description;
+  this.quantity = quantity;
+}
+
+function getProducts() {
+  const productsList = JSON.parse(fs.readFileSync("/database/products.json", "utf-8"));
+
+  return productsList.map
+  (product => 
+    new Product(
+      product.category, 
+      product.path, 
+      product.picture1, 
+      product.picture2, 
+      product.picture3, 
+      product.picture4, 
+      product.name, 
+      product.discount, 
+      product.oldPrice, 
+      product.price,
+      product.paymentConditions,
+      product.description, 
+      product.quantity
+      )
+    );
+}
+
+module.exports = {
+  getProducts,
+}
