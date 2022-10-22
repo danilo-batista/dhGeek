4bd075ddaab1900b6b5665c0d49d7437ce55b47f
alilopes <aliomar.lopesneto@gmail.com>
2022-10-22 16:25:22 -0300

    fixed responsivity in all views

--- src/models/product.js
+++ src/models/product.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require("path");
 
-function ProductModel(path, category, picture1, picture2, picture3, picture4, name, discount, oldPrice, price, paymentConditions, description, quantity) {
+function ProductModel(category, path, picture1, picture2, picture3, picture4, name, discount, oldPrice, price, paymentConditions, descriptionTitle, description, quantity) {
   this.category = category
   this.path = path
   this.picture1 = picture1;
@@ -13,6 +13,7 @@ function ProductModel(path, category, picture1, picture2, picture3, picture4, na
   this.oldPrice = oldPrice;
   this.price = price;
   this.paymentConditions = paymentConditions;
+  this.descriptionTitle = descriptionTitle;
   this.description = description;
   this.quantity = quantity;
 }
@@ -23,18 +24,19 @@ function getProducts() {
   return productsList.map
   (product => 
     new ProductModel(
-      product.category, 
-      product.path, 
-      product.picture1, 
-      product.picture2, 
-      product.picture3, 
-      product.picture4, 
-      product.name, 
+      product.category,
+      product.path,
+      product.picture1,
+      product.picture2,
+      product.picture3,
+      product.picture4,
+      product.name,
       product.discount, 
-      product.oldPrice, 
+      product.oldPrice,
       product.price,
       product.paymentConditions,
-      product.description, 
+      product.descriptionTitle,
+      product.description,
       product.quantity
       )
     );
