6f8678ae08b46fe01405c7e8286a393758852b34
Renanppcosta <renanppcosta@gmail.com>
2022-10-25 01:06:17 -0300

    header adjustments

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -1,6 +1,8 @@
 const hamburguerMenu = document.querySelector(".hamburguer-menu")
 const menu = document.getElementById("sidebar-menu")
 const closeMenu = document.querySelector(".close-menu")
+const cartButton = document.querySelector(".cart-button")
+const cartNumber = document.querySelector(".cart-button div")
 
 hamburguerMenu.addEventListener("click", ()=>{
     menu.style.display = "flex";
@@ -13,4 +15,12 @@ closeMenu.addEventListener("click", ()=>{
     menu.style.display = "none";
     hamburguerMenu.style.display = "inline";
     closeMenu.style.display = "none";
+})
+
+cartButton.addEventListener("mouseover", ()=>{
+    cartNumber.style.display = "block";
+})
+
+cartButton.addEventListener("mouseout", ()=>{
+    cartNumber.style.display = "none";
 })
