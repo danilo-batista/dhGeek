2735a0fdd550e6167c07c288f8c24afae1b3e2a4
Danilo Batista <dbatistadsn@gmail.com>
2022-11-04 07:58:12 -0300

    Fix: update the README.md and create the process with .env.

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,16 @@
-const  Sequelize  = require('sequelize');
+require('dotenv/config');
+const Sequelize = require('sequelize');
 
-const conection = new Sequelize("DHGEEK", "root", "1234567", {
-    host: 'localhost',
-    dialect: 'mysql'
+const conection = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: `${process.env.DB_CONNECTION}`
 });
 
 conection.authenticate()
-.then(function(){
-  console.log("successful database connection")
-}).catch(function(){
+  .then(function () {
+    console.log("successful database connection")
+  }).catch(function () {
     console.log("Error: Database connection not successful!");
-})
+  })
 
 module.exports = conection;
